918aee1c97d15a49a8febb33348cf03e9f7fc5a8
alice <alice@alysia.au>
2025-06-30 14:28:07 +1000

    added a script and updated configs

---
+++ install-apps.sh
@@ -0,0 +1,3 @@
+/usr/bin/paru install extra/niri extra/kcalc extra/ghostty extra/firefox extra/dolphin aur/kde-servicemenus-rootactions aur/hyprpicker aur/walker aur/swaylock extra/swaync extra/waybar extra/qt5ct aur/xdg-autostart extra/xdg-desktop-portal-gnome extra/xdg-desktop-portal-gtk extra/xdg-desktop-portal-kde extra/xdg-desktop-portal extra/blueman extra/gnome-keyring extra/polkit-kde-agent aur/waypaper extra/swaybg extra/xwayland-satellite
+cp -rf "./config files/*" .config
+sudo cp -rf ./autostart /etc/xdg/autostart
